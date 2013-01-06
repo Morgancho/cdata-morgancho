edbcd8ba803ae47749303a8ad83d5d50e9a14f10
Morgan Cho <morgancho2011@gmail.com>
2013-01-06 23:38:38 +0800

    exercise 11: Task queue

--- cdata.c
+++ cdata.c
@@ -1,90 +1,82 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/sched.h>    // 0-include header file
+#include <linux/sched.h>
 #include <linux/fs.h>
 #include <linux/miscdevice.h>
-#include <linux/wait.h>     // 0-include header file
-#include <linux/smp_lock.h> // 08-include header file for semaphore
+#include <linux/wait.h>
+#include <linux/tqueue.h>
 #include <asm/io.h>
 #include <asm/uaccess.h>
-
-#include "myioctl.h"
 
 #ifdef CONFIG_SMP
 #define __SMP__
 #endif
 
 #define	CDATA_MAJOR 121 
-#define BUFSIZE 1024
+#define BUFSIZE     1024
 
 #define LCD_WIDTH   (240)
 #define LCD_HEIGHT  (320)
 #define LCD_BPP     (4)
-#define LCD_SIZE    (LCD_WIDTH * LCD_HEIGHT * LCD_BPP)
+#define LCD_SIZE    (LCD_WIDTH*LCD_HEIGHT*LCD_BPP)
 
 struct cdata_t {
-   char         data[BUFSIZE];
-   int          index;
-   int          count;
-   int          offset;
+    char        data[BUFSIZE];
+    int         index;
+    char        *iomem;
+    unsigned int    offset;
+#if 0
+    struct timer_list   timer;
+#endif
+     struct tq_struct  tq;  //add for task queue
+     wait_queue_head_t   wait;
+};
 
-   wait_queue_head_t	wait;   // 1-must declare a wait queue by myself
-   spinlock_t		lock;
+static DECLARE_MUTEX(cdata_sem);
 
-   char         *iomem;
-   struct timer_list    timer;
-
-};	//semicolon must be added otherwise unexpected error may happen
-
-static DECLARE_MUTEX(cdata_sem);  // 18-declare semaphore and initial
-
-static cdata_mmap(struct file *filp, struct vm_area_struct *vma)
+// Supporting APIs
+void flush_lcd(unsigned long priv)
 {
-   unsigned long start = vma->vm_start;
-   unsigned long end = vma->vm_end;
-   unsigned long size = end - start;
+    struct cdata_t *cdata = (struct cdata_t *)priv;
+    char *fb = cdata->iomem;
+    int offset = cdata->offset;
+    int index = cdata->index;
+    int i;
 
-   remap_page_range(start, 0x33f00000, size, PAGE_SHARED);
-}
+    for (i = 0; i < index; i++) {
+        writeb(cdata->data[i], fb + offset);
+        offset++;
 
-void flush_lcd (unsigned long priv)
-{
-        struct cdata_t *cdata = (struct cdata_t *)priv;
-        char *fb = cdata->iomem;
-        int index = cdata->index;
-        int offset = cdata->offset;
-        int i;
+        if (offset >= LCD_SIZE)
+            offset = 0;
+    }
 
-        for (i=0; i<index; i++) {
-            writeb(cdata->data[i], fb+offset);
-            offset++;
-            if(offset >= LCD_SIZE)
-               offset = 0;
-        }
-        cdata->index =0;
-        cdata->offset = offset;
-        
-        // wake up process
-        //current->state = TASK_RUNNING;   // we don't know where "current" point to at this moment
-        wake_up(&cdata->wait);
+    cdata->index = 0;
+    cdata->offset = offset;
+
+    // Wake up process
+    current->state = TASK_RUNNING;
+    wake_up(&cdata->wait);
 }
 
 static int cdata_open(struct inode *inode, struct file *filp)
 {
-	struct cdata_t *cdata;
+    struct cdata_t *cdata;
 
-	printk(KERN_ALERT "cdata: in cdata_open()\n");
+    cdata = (struct cdata_t *)kmalloc(sizeof(struct cdata_t), GFP_KERNEL);
+    cdata->index = 0;
 
-	cdata = (struct cdata_t *)kmalloc(sizeof(struct cdata_t), GFP_KERNEL);
-	cdata->index = 0;	// kmalloc won't init the memory to 0
-	cdata->count = 0;
-	filp->private_data = (void *)cdata;
+    init_waitqueue_head(&cdata->wait);
+    cdata->iomem = ioremap(0x33f00000, LCD_SIZE);
+#if 0
+    init_timer(&cdata->timer);
+#endif
+    INIT_TQUEUE(&cdata->tq, flush_lcd, (void *)cdata);	//add for task queue
 
-        init_waitqueue_head(&cdata->wait);   // 2-init wait queue head
+    cdata->offset = 0;
 
-        cdata->iomem = ioremap(0x33f00000, LCD_SIZE);
-        init_timer(&cdata->timer);   //initial timer
+    filp->private_data = (void *)cdata;
 
 	return 0;
 }
@@ -92,83 +84,51 @@ static int cdata_open(struct inode *inode, struct file *filp)
 static int cdata_ioctl(struct inode *inode, struct file *filp, 
 			unsigned int cmd, unsigned long arg)
 {
-	struct cdata_t *cdata;
-	int i;
-	cdata = (struct cdata_t *)filp->private_data;
-
 	printk(KERN_ALERT "cdata: in cdata_ioctl()\n");
-        switch (cmd)
-	{
-	   case CDATA_EMPTY:
-	      //printk(KERN_ALERT "cdata_ioctl: IOCTL_EMPTY\n");
-	      printk(KERN_ALERT "cdata_ioctl: %p\n", filp);
 
-	      for (i=0; i<cdata->count; i++)
-	         cdata->data[i] = 0;
-
-	      break;
-	   case CDATA_SYNC:
-	      printk(KERN_ALERT "cdata_ioctl: IOCTL_SYNC\n");
-
-	      for (i=0; i <cdata->count; i++)
-	         printk(KERN_ALERT "data[%i]=%c\n", i, cdata->data[i]);
-
-	      break;
-	   default:
-	      return -ENOTTY;
-	}
-        
-        return 0;
+    return 0;
 }
 
 static ssize_t cdata_read(struct file *filp, char *buf, 
 				size_t size, loff_t *off)
 {
-        struct cdata_t *cdata;        
-        cdata = (struct cdata_t *)filp->private_data;
-
-        printk(KERN_ALERT "cdata: in cdata_read()\n");
-        return 0;
+	printk(KERN_ALERT "cdata: in cdata_read()\n");
+    return 0;
 }
 
 static ssize_t cdata_write(struct file *filp, const char *buf, 
 				size_t count, loff_t *off)
 {
-	struct cdata_t *cdata = (struct cdata_t *)filp->private_data;
-        DECLARE_WAITQUEUE(wait, current);   // 3-create a node with current process
-	int i;
+    struct cdata_t *cdata = (struct cdata_t *)filp->private_data;
+    DECLARE_WAITQUEUE(wait, current);
+    int i;
 
-	printk(KERN_ALERT "cdata_write: %s\n", buf);
+    down(&cdata_sem);
+    for (i = 0; i < count; i++) {
+        if (cdata->index >= BUFSIZE) {
+            add_wait_queue(&cdata->wait, &wait);
+            current->state = TASK_INTERRUPTIBLE;
 
-	cdata->count = count;
-	
-	//mutex_lock
-        down(&cdata_sem);   // 28- lock semaphore
+            /*
+             */
+#if 0
+            cdata->timer.expires = jiffies + 500;
+            cdata->timer.function = flush_lcd;
+            cdata->timer.data = (unsigned long)cdata;
+            add_timer(&cdata->timer);
+#endif
+            schedule_task(&cdata->tq);  //add for task queue
+       
+            schedule();
 
-	for(i=0; i<count; i++) {
-	   if(cdata->index > BUFSIZE)
-              add_wait_queue(&cdata->wait, &wait);   // 4-add the node into wait queue
-              current->state = TASK_INTERRUPTIBLE;   // 5-state change to waiting
+            current->state = TASK_RUNNING;
+            remove_wait_queue(&cdata->wait, &wait);
+        }
 
-              up(&cdata_sem);   // add before schedule() for atomic of critical section
-
-              cdata->timer.expires = jiffies + 500;
-              cdata->timer.function = flush_lcd;
-              cdata->timer.data = (unsigned long)cdata;
-              add_timer(&cdata->timer);
-
-              schedule();   // 6-call scheduler to take over
-              down(&cdata_sem); // add after schedule() for atomic of critical section
-
-              current->state = TASK_RUNNING;   // 7-at this moment, current process is back and re-gain the cpu control
-              remove_wait_queue(&cdata->wait, &wait);  // 8-remove our node from wait queue
-	      return -EFAULT;
-	   if(copy_from_user(&cdata->data[cdata->index++], &buf[i], 1))
-	      return -EFAULT;
-	}
-
-	//mutex_unlock
-        up(&cdata_sem);   // 38- unlock semaphore
+        if (copy_from_user(&cdata->data[cdata->index++], &buf[i], 1))
+            return -EFAULT;
+    }
+    up(&cdata_sem);
 
 	return 0;
 }
@@ -179,31 +139,32 @@ static int cdata_release(struct inode *inode, struct file *filp)
 	return 0;
 }
 
+static int cdata_mmap(struct file *filp, struct vm_area_struct *vma)
+{
+    unsigned long start = vma->vm_start;
+    unsigned long end = vma->vm_end;
+    unsigned long size;
+
+    size = end - start;
+    //remap_page_range(start, 0x33f00000, size, PAGE_SHARED);
+}
+
 struct file_operations cdata_fops = {	
 	open:		cdata_open,
 	release:	cdata_release,
 	ioctl:		cdata_ioctl,
 	read:		cdata_read,
 	write:		cdata_write,
-        mmap:           cdata_mmap,
+    mmap:       cdata_mmap,
 };
 
 int my_init_module(void)
 {
-        //int i;	
-        //char *fb;
+    int i;
 
-        if (register_chrdev(CDATA_MAJOR, "cdata", &cdata_fops)) {        
-	   printk(KERN_ALERT "cdata module: cannot registered.\n");
-        }
-
-/*
-        fb = ioremap(0x33f00000, 400);
-        for (i=0; i<100; i++) {
-           writel(0x00ff0000, fb);
-           fb += 4;
-        }
-*/
+	if (register_chrdev(CDATA_MAJOR, "cdata", &cdata_fops)) {
+	    printk(KERN_ALERT "cdata module: can't registered.\n");
+    }
 
 	return 0;
 }
